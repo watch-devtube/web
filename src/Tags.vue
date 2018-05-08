cee60ca5b26e96fd8edcde8c831291abf399fa9f
Andrey Adamovich <2787794+aadamovich@users.noreply.github.com>
2018-05-08 23:59:47 +0300

    Fixed field name

--- src/Tags.vue
+++ src/Tags.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .tags
-    span.tag.is-primary.is-uppercase(v-if="isNew(addedAt)") New
+    span.tag.is-primary.is-uppercase(v-if="isNew(creationDate)") New
     a.tag.is-uppercase(v-for="(tag) in tags" v-on:click="refineTag(tag)") {{tag}}
     a.tag.is-uppercase(v-on:click="refineChannel(channel)") 
       i.fab.fa-youtube 
@@ -13,15 +13,15 @@
     props: { 
       tags: { required: true },
       channel: { required: true },
-      addedAt: { required: true }
+      creationDate: { required: true }
     },
     mixins: [Component],
     computed: {
     },
     methods: {
-      isNew: function(addedAt) {
+      isNew: function(creationDate) {
         let today = new Date().getTime()
-        return (today - addedAt) / (24*12*60*1000) < 14
+        return (today - creationDate*1000) / (24*12*60*1000) < 14
       },
       refineTag: function (tag) {
         this.searchStore.algoliaHelper.addDisjunctiveFacetRefinement('tags', tag)
