21f764639b3c507e76c3298c245e297e72218fdb
Andrey Adamovich <andrey@aestasit.com>
2018-07-15 00:00:55 +0300

    hmm, seems likes a bug in serverless

--- express/index.ts
+++ express/index.ts
@@ -8,3 +8,11 @@ export function proxy(req, res) {
   }
   return app(req, res)
 }
+
+export function proxy2(req, res) {
+  if (!req.url) {
+    req.url = '/'
+    req.path = '/'
+  }
+  return app(req, res)
+}
