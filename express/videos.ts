ef8335ac089ecf7bffe676d52a8af94bac1bb085
Eduards Sizovs <eduardsi@Eduardss-MacBook-Pro.local>
2018-08-23 17:40:41 +0300

    preserve the right video ordering when fetched from datastore

--- express/videos.ts
+++ express/videos.ts
@@ -5,16 +5,20 @@ let datastore = new Datastore({})
 export class Videos {
 
   videoKeys: any[]
+  ids: string[]
 
   constructor(ids) {
-    this.videoKeys = ids.map(id => datastore.key(['video', id.objectID || id]))
+    this.ids = ids
+    this.videoKeys = ids.map(id => datastore.key(['video', id]))
   }
 
   fetch() {
     if (!this.videoKeys.length) {
       return Promise.resolve([])
     }
-    return datastore.get(this.videoKeys).then(([videos]) => videos)
+    return datastore.get(this.videoKeys)
+      .then(([videos]) => videos)
+      .then(videos => videos.sort((a,b) => this.ids.indexOf(a.objectID) - this.ids.indexOf(b.objectID) ))
   }
 
 }
