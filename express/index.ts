9f894458e6f2202fb8c4506f1cfa7c86b8c9f091
Andrey Adamovich <andrey@aestasit.com>
2018-09-02 21:06:17 +0300

    handler matches the path

--- express/index.ts
+++ express/index.ts
@@ -1,7 +1,15 @@
 
 var app = require('./src/app')
 
-export function proxy(req, res) {
+export function proxyBlue(req, res) {
+  if (!req.url) {
+    req.url = '/'
+    req.path = '/'
+  }
+  return app(req, res)
+}
+
+export function proxyGreen(req, res) {
   if (!req.url) {
     req.url = '/'
     req.path = '/'
