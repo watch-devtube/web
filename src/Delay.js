bab59f1b85a84c3306a147030a91d30e7937c93e
Eduards Sizovs <eduards.sizovs@gmail.com>
2018-12-13 17:42:34 +0200

    add missing Delay component

---
+++ src/Delay.js
@@ -0,0 +1,39 @@
+export default {
+  name: 'Delay',
+
+  props: {
+    wait: {
+      type: Number,
+      default: 0
+    },
+    from: {
+      type: Number,
+      default: Date.now()
+    }
+  },
+
+  data: () => ({
+    waiting: true
+  }),
+
+  created() {
+    this.timer = setTimeout(() => {
+      this.waiting = false
+    }, this.from - Date.now() + this.wait)
+  },
+
+  destroyed() {
+    clearTimeout(this.timer)
+  },
+
+  render(h) {
+    return h(
+      'div',
+      this.waiting ? this.$slots.loading || null : this.$slots.default
+    )
+  },
+
+  install(Vue, name) {
+    Vue.component(name || 'Delay', this)
+  }
+}
