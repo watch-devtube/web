13c84df02a9176b524198899d2ec2fa86c11e561
Eduards Sizovs <eduards@devternity.com>
2020-04-06 19:01:53 +0700

    add night mode style to sort dropdown

--- src/Sorting.vue
+++ src/Sorting.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
-  .xxx
-    .select.is-small(@change="sort(sorting)")
-      select(v-model="sorting")
-        option(value="-satisfaction" :selected="(query.sortOrder == '-satisfaction' || query.sortOrder == '-featured')") Top first
-        option(value="-recordingDate" :selected="(query.sortOrder == '-recordingDate')") New first
-        option(value="duration" :selected="(query.sortOrder == 'duration')") Shortest first
+  .select.is-small(@change="sort(sorting)")
+    select(v-model="sorting")
+      option(value="-satisfaction" :selected="(query.sortOrder == '-satisfaction' || query.sortOrder == '-featured')") Top first
+      option(value="-recordingDate" :selected="(query.sortOrder == '-recordingDate')") New first
+      option(value="duration" :selected="(query.sortOrder == 'duration')") Shortest first
 </template>
 <script>
   import { mapState, mapActions, mapGetters } from 'vuex'
