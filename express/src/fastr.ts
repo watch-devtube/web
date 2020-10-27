2170134cb99aac4bdfb26ca66168639a0fc3a774
Eduards Sizovs <eduards@devternity.com>
2020-10-27 20:51:46 +0200

    placeholder code for separate loki/lunr loading

--- express/src/fastr.ts
+++ express/src/fastr.ts
@@ -2,14 +2,26 @@ import { Fastr } from "devtube-commons";
 import { Timer } from "./timer";
 
 const path = require("path");
+const cwd = process.cwd();
 
-console.log(`Cwd: ${process.cwd()}`);
-
-const dataDir = path.resolve(process.cwd(), "data");
-console.log(dataDir);
+const { lokiDir, lunrDir } = process.env;
 
 const fastrInitTime = new Timer("fastr init").withWarningIfSlow();
-const fastr = new Fastr({ dataDir, serialized: true });
+
+let fastr: Fastr;
+if (lokiDir) {
+  // uncomment after commons upgrade
+  // fastr = new Fastr({ lokiDir: path.resolve(cwd, lokiDir), serialized: true });
+} else if (lunrDir) {
+  // uncomment after commons upgrade
+  // fastr = new Fastr({ lunrDir: path.resolve(cwd, lunrDir), serialized: true });
+} else {
+  // Backward compatibility with a "fat" lambda that includes
+  // both loki and lundr. Remove after migration.
+  fastr = new Fastr({ dataDir: path.resolve(cwd, "data"), serialized: true });
+}
+
+
 fastrInitTime.print();
 
 module.exports = fastr;
