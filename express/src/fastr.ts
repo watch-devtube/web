898fe416eec94327c98c30bdcaba8fe01ca6b963
Andrey Adamovich <andrey@aestasit.com>
2020-10-27 22:36:06 +0200

    split lunr/loki loading

--- express/src/fastr.ts
+++ express/src/fastr.ts
@@ -11,13 +11,13 @@ const fastrInitTime = new Timer("fastr init").withWarningIfSlow();
 let fastr: Fastr;
 if (lokiDir) {
   // uncomment after commons upgrade
-  // fastr = new Fastr({ lokiDir: path.resolve(cwd, lokiDir), serialized: true });
+  fastr = new Fastr({ lokiDir: path.resolve(cwd, lokiDir), serialized: true });
 } else if (lunrDir) {
   // uncomment after commons upgrade
-  // fastr = new Fastr({ lunrDir: path.resolve(cwd, lunrDir), serialized: true });
+  fastr = new Fastr({ lunrDir: path.resolve(cwd, lunrDir), serialized: true });
 } else {
   // Backward compatibility with a "fat" lambda that includes
-  // both loki and lundr. Remove after migration.
+  // both loki and lunr. Remove after migration.
   fastr = new Fastr({ dataDir: path.resolve(cwd, "data"), serialized: true });
 }
 
