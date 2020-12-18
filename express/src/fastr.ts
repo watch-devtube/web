0ef958db46b6f2f610818e97873c79626155ebe0
Eduards Sizovs <eduards@devternity.com>
2020-12-19 01:35:09 +0200

    ditch multi-lambda deployments

--- express/src/fastr.ts
+++ express/src/fastr.ts
@@ -4,20 +4,9 @@ import * as path from "path";
 
 const cwd = process.cwd();
 
-const { lokiDir, lunrDir } = process.env;
-
 const fastrInitTime = new Timer("fastr init").withWarningIfSlow();
 
-let fastr: Fastr;
-if (lokiDir) {
-  fastr = new Fastr({ lokiDir: path.resolve(cwd, lokiDir), serialized: true });
-} else if (lunrDir) {
-  fastr = new Fastr({ lunrDir: path.resolve(cwd, lunrDir), serialized: true });
-} else {
-  // A "fat" lambda that includes both loki and lunr data. Used for local development.
-  fastr = new Fastr({ dataDir: path.resolve(cwd, "data"), serialized: true });
-}
-
+const fastr = new Fastr({ dataDir: path.resolve(cwd, "data"), serialized: true });
 
 fastrInitTime.print();
 
