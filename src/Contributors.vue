f5cacb83b4a3d452117a3a96e09e090579a55bb6
Eduards Sizovs <eduards@devternity.com>
2020-12-19 22:06:29 +0200

    ditch tag  contributions

--- src/Contributors.vue
+++ src/Contributors.vue
@@ -1,50 +1,58 @@
 <template lang="pug">
-  .contributors
-    .section.container
-      .content
-        p
-          strong
-            a(href="https://github.com/watch-devtube/contrib" target="_blank") Contribution guideline
-        p
-          font-awesome-icon(:icon="['far', 'heart']").has-text-danger
-          |   5000 karma points – we'll thank you on Twitter
-        p
-          font-awesome-icon(:icon="['far', 'heart']").has-text-danger
-          |   10000 karma points — you can make any good video featured on the front page for a week.
-      .columns.is-multiline.is-mobile
-        .column.is-3-widescreen.is-4-tablet(v-for="contributor in contributors" v-if="contributor.name")
-          .card
-            .card-content
-              .media
-                .media-left
-                  figure.image.is-48x48
-                    img.avatar(:src="contributor.avatar")
-                .media-content
-                  p.title.is-6.is-capitalized {{contributor.name}}
-                  p.subtitle.is-7
-                    a(:href="'https://github.com/' + contributor.login" target="_blank") @{{contributor.login}}
-              nav.level.is-mobile
-                .level-item.has-text-centered
-                  .item
-                    p.heading.is-capitalized Pulls
-                    p.title.is-size-7 {{contributor.pullRequests}}
-                .level-item.has-text-centered
-                  .item
-                    p.heading.is-capitalized Videos
-                    p.title.is-size-7 {{contributor.videosUpdated}}
-                .level-item.has-text-centered
-                  .item
-                    p.heading.is-capitalized: font-awesome-icon(:icon="['fab', 'youtube']")
-                    p.title.is-size-7 {{contributor.channelContributions}}
-                .level-item.has-text-centered
-                  .item
-                    p.heading.is-capitalized: font-awesome-icon(:icon="['far', 'user-circle']")
-                    p.title.is-size-7 {{contributor.speakerContributions}}
-                .level-item.has-text-centered
-                  .item
-                    p.heading.is-capitalized: font-awesome-icon(:icon="['fas', 'hashtag']")
-                    p.title.is-size-7 {{contributor.tagContributions}}
-              ContributorRank(:karma="contributor.karma")
+.contributors
+  .section.container
+    .content
+      p
+        strong
+          a(href="https://github.com/watch-devtube/contrib", target="_blank") Contribution guideline
+      p
+        font-awesome-icon.has-text-danger(:icon="['far', 'heart']")
+        |
+        | 5000 karma points – we'll thank you on Twitter
+      p
+        font-awesome-icon.has-text-danger(:icon="['far', 'heart']")
+        |
+        | 10000 karma points — you can make any good video featured on the front page for a week.
+    .columns.is-multiline.is-mobile
+      .column.is-3-widescreen.is-4-tablet(
+        v-for="contributor in contributors",
+        v-if="contributor.name"
+      )
+        .card
+          .card-content
+            .media
+              .media-left
+                figure.image.is-48x48
+                  img.avatar(:src="contributor.avatar")
+              .media-content
+                p.title.is-6.is-capitalized {{ contributor.name }}
+                p.subtitle.is-7
+                  a(
+                    :href="'https://github.com/' + contributor.login",
+                    target="_blank"
+                  ) @{{ contributor.login }}
+            nav.level.is-mobile
+              .level-item.has-text-centered
+                .item
+                  p.heading.is-capitalized Pulls
+                  p.title.is-size-7 {{ contributor.pullRequests }}
+              .level-item.has-text-centered
+                .item
+                  p.heading.is-capitalized Videos
+                  p.title.is-size-7 {{ contributor.videosUpdated }}
+              .level-item.has-text-centered
+                .item
+                  p.heading.is-capitalized: font-awesome-icon(
+                    :icon="['fab', 'youtube']"
+                  )
+                  p.title.is-size-7 {{ contributor.channelContributions }}
+              .level-item.has-text-centered
+                .item
+                  p.heading.is-capitalized: font-awesome-icon(
+                    :icon="['far', 'user-circle']"
+                  )
+                  p.title.is-size-7 {{ contributor.speakerContributions }}
+            ContributorRank(:karma="contributor.karma")
 </template>
 <style lang="scss" scoped>
 header {
