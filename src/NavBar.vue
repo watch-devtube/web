d634cf4da823a1d38bc69e9c4aa2de4129994c07
Eduards Sizovs <eduards@devternity.com>
2022-04-24 17:00:29 +0700

    remove auth headers

--- src/NavBar.vue
+++ src/NavBar.vue
@@ -17,43 +17,11 @@ section.section
           target="_blank",
           ref="noopener"
         ) by DevTernity
-      .column
+      .column.is-hidden
         .is-pulled-right
-          .columns.is-mobile.is-vcentered.is-size-5.is-size-7-mobile(
-            v-if="auth.user"
-          )
+          .columns.is-mobile.is-vcentered.is-size-5.is-size-7-mobile
             .column.is-narrow
-              router-link.nav-link(
-                v-bind:class="{ 'nav-active': $route.name === 'subscriptions' }",
-                v-if="auth.user && hasSubscriptions",
-                :to="{ name: 'subscriptions' }"
-              ): strong.is-lowercase Subscriptions
-            .column.is-narrow
-              router-link.nav-link(
-                v-bind:class="{ 'nav-active': $route.name === 'watched' }",
-                title="Watched",
-                :to="{ name: 'watched' }"
-              ): strong watched
-            .column.is-narrow
-              router-link.nav-link(
-                v-bind:class="{ 'nav-active': $route.name === 'favorites' }",
-                title="Favorites",
-                :to="{ name: 'favorites' }"
-              ): strong favorites
-            .column.is-narrow
-              .columns.is-gapless.is-mobile.is-vcentered
-                .column.is-narrow.is-hidden-mobile
-                  .title.is-size-7
-                    font-awesome-icon.has-text-danger(:icon="['far', 'heart']")
-                    |
-                    | {{ karma.karma }}
-                    |
-                .column.is-narrow.is-hidden-mobile
-                  .face(
-                    :style="'background-image: url(' + auth.user.photoUrl + ')'"
-                  ) 
-                .column.is-narrow
-                  a.title.is-size-7(@click="signOut()") logout
+              .button Add video
 </template>
 <style lang="scss" scoped>
 .face {
