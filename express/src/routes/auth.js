807eb909152940ad07e43851e3ab58868a186730
Eduards Sizovs <eduards@devternity.com>
2022-05-01 19:55:32 +0700

    github auth done

--- express/src/routes/auth.js
+++ express/src/routes/auth.js
@@ -3,27 +3,29 @@ const passport = require("passport");
 const TwitterStrategy = require('passport-twitter');
 const GitHubStrategy = require('passport-github2');
 
-const devtubeHost = process.env.DEVTUBE_HOST || 'https://dev.tube'
+const successRedirect = process.env.DEVTUBE_HOST || 'https://dev.tube'
+
+function toUserProfile(_token, _tokenSecret, profile, cb) {
+  const avatar = profile.photos[0].value
+  const email = profile.emails[0].value
+  if (!avatar || !email) {
+    throw "Sorry, unable to retrieve user data from OAuth provider"
+  }
+  const user = {
+    avatar,
+    email,
+    username: profile.username,
+    provider: profile.provider
+  };
+  return cb(null, user);
+}
 
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
   callbackURL: "/auth/github/callback"
 },
-  function verify(_token, _tokenSecret, profile, cb) {
-    const avatar = profile.photos[0].value
-    const email = profile.emails[0].value
-    if (!avatar || !email) {
-      throw "Sorry, unable to retrieve data from Twitter"
-    }
-    const user = {
-      avatar,
-      email,
-      username: profile.username,
-      provider: profile.provider
-    };
-    return cb(null, user);
-  }
+  toUserProfile
 ));
 
 passport.use(new TwitterStrategy({
@@ -33,20 +35,7 @@ passport.use(new TwitterStrategy({
   proxy: true,
   includeEmail: true
 },
-  function verify(_token, _tokenSecret, profile, cb) {
-    const avatar = profile.photos[0].value
-    const email = profile.emails[0].value
-    if (!avatar || !email) {
-      throw "Sorry, unable to retrieve data from Twitter"
-    }
-    const user = {
-      avatar,
-      email,
-      username: profile.username,
-      provider: profile.provider
-    };
-    return cb(null, user);
-  }));
+  toUserProfile));
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
@@ -71,22 +60,13 @@ router.get('/logout', (req, res) => {
 
 router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
 router.get('/github/callback',
-  passport.authenticate('github', { failureRedirect: '/' }),
-  (req, res) => {
-    res.redirect(devtubeHost);
-  });
-
+  passport.authenticate('github', { successRedirect, failureRedirect: '/' }));
 
 router.get('/twitter', passport.authenticate('twitter'));
 router.get('/twitter/callback',
-  passport.authenticate('twitter', { assignProperty: 'twitterProfile', failureRedirect: '/' }),
-  (req, res, next) => {
-    req.login(req.twitterProfile, (err) => {
-      if (err) { return next(err); }
-      res.setHeader('Cache-Control', 'private');  // Recommended by Firebase https://stackoverflow.com/questions/44929653/firebase-cloud-function-wont-store-cookie-named-other-than-session
-      res.redirect(devtubeHost);
-    });
-  });
+  passport.authenticate('twitter', { successRedirect, failureRedirect: '/' }));
+
+
 
 
 module.exports = router;
