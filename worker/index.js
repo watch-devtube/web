a25cb486ff5707aef8e1e3823385ef099a8b60eb
Eduards Sizovs <eduards@devternity.com>
2022-06-03 19:40:23 +0800

    fix rss/sitemap worker

--- worker/index.js
+++ worker/index.js
@@ -6,10 +6,10 @@ async function handleRequest(request) {
   const { url } = request
 
   if (url.includes('dev.tube/sitemap.xml')) {
-    return Response.redirect('api.dev.tube/sitemap', 302)
+    return Response.redirect('https://api.dev.tube/sitemap', 302)
   } else if (url.includes('dev.tube/rss.xml')) {
-    return Response.redirect('api.dev.tube/rss', 302)
+    return Response.redirect('https://api.dev.tube/rss', 302)
   } else {
     return await fetch(request)
   }
-}
+} 
