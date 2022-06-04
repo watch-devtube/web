93e0950247cfd8a6fa55da51a60e618b375a7c85
Eduards Sizovs <eduards@devternity.com>
2022-06-05 00:41:44 +0800

    add title attr to footer

--- vue/src/Footer.vue
+++ vue/src/Footer.vue
@@ -11,8 +11,8 @@ footer.footer.has-background-white
         a(href='https://devternity.com' target='_blank') DevTernity 
         | &ndash; the top software developer conference.
     p.is-size-5
-      a.mr-3.has-text-grey-dark(href='https://github.com/watch-devtube/web' rel="nofollow noopener noreferrer"): font-awesome-icon(:icon="['fab', 'github']")
-      a.mr-3.has-text-grey-dark(href='https://twitter.com/watchdevtube' rel="nofollow noopener noreferrer"): font-awesome-icon(:icon="['fab', 'twitter']")
-      a.mr-3.has-text-grey-dark(href='mailto:hello@dev.tube'): font-awesome-icon(:icon="['fa', 'envelope']")
-      a.mr-3.has-text-grey-dark(href='https://dev.tube/rss.xml'): font-awesome-icon(:icon="['fa', 'square-rss']")
+      a.mr-3.has-text-grey-dark(href='https://github.com/watch-devtube/web' rel="nofollow noopener noreferrer"): font-awesome-icon(:icon="['fab', 'github']" title="Our GitHub")
+      a.mr-3.has-text-grey-dark(href='https://twitter.com/watchdevtube' rel="nofollow noopener noreferrer"): font-awesome-icon(:icon="['fab', 'twitter']" title="Our Twitter")
+      a.mr-3.has-text-grey-dark(href='mailto:hello@dev.tube'): font-awesome-icon(:icon="['fa', 'envelope']" title="Our Email")
+      a.mr-3.has-text-grey-dark(href='https://dev.tube/rss.xml'): font-awesome-icon(:icon="['fa', 'square-rss']" title="Subscribe to RSS feed")
 </template>
