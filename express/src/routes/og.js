b5ace1398a909db0fa3cff99c7056a906b5bf427
Eduards Sizovs <eduards@devternity.com>
2022-06-16 12:39:32 +0700

    make og images look nicer with longer talk titles

--- express/src/routes/og.js
+++ express/src/routes/og.js
@@ -39,14 +39,14 @@ router.get(
 
 function ogImage(video, watchingNow, comments) {
   const imageDomain = "https://res.cloudinary.com/eduardsi/image/upload";
-  const imageTemplate = 'devtube_card_fivpsf'
+  const imageTemplate = 'devtube_card_whhycj'
 
   const image =
     imageDomain +
     "/w_1000,c_fit,co_white,l_text:Lato_60_bold:" + doubleEscape(video.title) + ",g_north_west,x_100,y_100" +
-    "/w_1000,c_fit,co_white,l_text:Lato_30:" + kilo(video.likes) + " likes" + ",g_north_west,x_140,y_232" +
-    "/w_1000,c_fit,co_white,l_text:Lato_30:" + kilo(watchingNow) + " watching now" + ",g_north_west,x_428,y_232" +
-    "/w_1000,c_fit,co_white,l_text:Lato_30:" + kilo(comments) + " comments" + ",g_north_west,x_855,y_232" +
+    "/w_1000,c_fit,co_white,l_text:Lato_30:" + kilo(video.likes) + " likes" + ",g_north_west,x_140,y_332" +
+    "/w_1000,c_fit,co_white,l_text:Lato_30:" + kilo(watchingNow) + " watching now" + ",g_north_west,x_428,y_332" +
+    "/w_1000,c_fit,co_white,l_text:Lato_30:" + kilo(comments) + " comments" + ",g_north_west,x_855,y_332" +
     "/w_1000,c_fit,co_white,l_text:Lato_40_bold:" + video.speakerNames[0] + ",g_north_west,x_240,y_539" +
     "/w_1000,c_fit,co_grey,l_text:Lato_30_bold:" + "@" + video.speakerTwitters[0] + ",g_north_west,x_240,y_587" +
     "/l_twitter_name:" + video.speakerTwitters[0] + ".png,w_96,r_max,g_north_west,x_100,y_515,bo_5px_solid_white" +
